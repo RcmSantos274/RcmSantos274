efe202ed1a61afe2dd156027a25f4d6cded3f909
RcmSantos274 <136946716+RcmSantos274@users.noreply.github.com>
2023-09-13 16:18:21 -0300

    Update 347.c

--- ExerciciosMonitoria/347.c
+++ ExerciciosMonitoria/347.c
@@ -3,18 +3,18 @@
 #include <stdbool.h>
 #include <string.h>
 
-// Definição da estrutura para representar pares (int, int)
+// definição da estrutura para representar pares (int, int)
 struct Par {
     int primeiro;
     int segundo;
 };
 
-// Função de comparação para o heap (min-heap)
+// função de comparação para o heap (min-heap)
 int comparar(const void *a, const void *b) {
     return ((struct Par *)a)->primeiro - ((struct Par *)b)->primeiro;
 }
 
-// Função para criar um novo par (int, int)
+// função para criar um novo par (int, int)
 struct Par criarPar(int primeiro, int segundo) {
     struct Par par;
     par.primeiro = primeiro;
@@ -23,16 +23,16 @@ struct Par criarPar(int primeiro, int segundo) {
 }
 
 int* Frequentes(int* numeros, int tamanho, int k, int* tamanhoRetornado) {
-    // Contagem de frequência dos números usando um dicionário simples (array)
-    int maxNum = 10001; // Assumindo que os números estão no intervalo [0, 10000]
+    // contagem de frequência dos números usando um dicionário simples (array)
+    int maxNum = 10001; // assumindo que os números estão no intervalo [0, 10000]
     int frequencia[maxNum];
-    memset(frequencia, 0, sizeof(frequencia));
+    memset(frequencia, 0, sizeof(frequencia)); // preencher o vetor com 0 para que todos sejam sem alunos inicialmente
 
     for (int i = 0; i < tamanho; i++) {
         frequencia[numeros[i]]++;
     }
 
-    // Criando um heap (min-heap) para armazenar os K elementos mais frequentes
+    // criando um heap (min-heap) para armazenar os K elementos mais frequentes
     struct Par heap[k];
     int tamanhoHeap = 0;
 
@@ -41,18 +41,18 @@ int* Frequentes(int* numeros, int tamanho, int k, int* tamanhoRetornado) {
             if (tamanhoHeap < k) {
                 heap[tamanhoHeap++] = criarPar(frequencia[i], i);
                 if (tamanhoHeap == k) {
-                    // Construir o heap (min-heap) usando qsort
+                    // construir o heap (min-heap) usando qsort
                     qsort(heap, k, sizeof(struct Par), comparar);
                 }
             } else if (frequencia[i] > heap[0].primeiro) {
                 heap[0] = criarPar(frequencia[i], i);
-                // Reordenar o heap (min-heap) após a substituição
+                // reordenar o heap (min-heap) após a substituição
                 qsort(heap, k, sizeof(struct Par), comparar);
             }
         }
     }
 
-    // Criando o vetor de resposta com os K elementos mais frequentes
+    // criando o vetor de resposta com os K elementos mais frequentes
     int* resposta = (int*)malloc(k * sizeof(int));
     for (int i = 0; i < k; i++) {
         resposta[i] = heap[i].segundo;
